d497ada7190663202aeb40df4e4b27f584f85830
Alice <aliceevelynashby@gmail.com>
2020-06-02 14:48:44 +0100

    created UI manager class

---
+++ UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    private Scene currentScene;
+
+    void Start() { currentScene = SceneManager.GetActiveScene(); }
+
+    public void ChangeScene()
+    {
+        Debug.Log("current scene before: " + (currentScene.buildIndex));
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+        Debug.Log("current scene after: " + (currentScene.buildIndex));
+    }
+
+    /*void Update() {
+        // this is a temporary measure until logic has been coded into the game
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("current scene: " + (currentScene.buildIndex));
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+    }
+
+    public void PlayGame() {
+
+        if (currentScene.name == "StartScreen") 
+        {
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+        else if (currentScene.name == "LevelOne") {
+            SceneManager.LoadScene(currentScene.name);
+        }
+
+    }
+
+    public void QuitGame() {
+
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+
+    }*/
+
+}
