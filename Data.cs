ac3bdcac94b6aaba5c9f42347badbe49a605b5b4
Alice <aliceevelynashby@gmail.com>
2020-06-02 14:45:59 +0100

    created data class

---
+++ Data.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Data 
+{
+    private int mWaterSustainability;
+    public int WaterSustainability
+    {
+        get {
+            return mWaterSustainability;
+        }
+        set {
+            if (value >= 0)
+            {
+                mWaterSustainability = value;
+            }
+        }
+    }
+
+    private int mEnvironment;
+    public int Environment
+    {
+        get {
+            return mEnvironment;
+        }
+        set {
+            if (value >= 0)
+            {
+                mEnvironment = value;
+            }
+        }
+    }
+
+    private int mEconomy;
+    public int Economy
+    {
+        get {
+            return mEconomy;
+        }
+        set {
+            if (value >= 0)
+            {
+                mEconomy = value;
+            }
+        }
+    }
+
+    private int mSociety;
+    public int Society
+    {
+        get {
+            return mSociety;
+        }
+        set {
+            if (value >= 0)
+            {
+                mSociety = value;
+            }
+        }
+    }
+
+
+}
