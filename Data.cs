61134b7823e012d964e7c26c7a7e61ce7190d3f7
Alice <aliceevelynashby@gmail.com>
2020-06-04 00:29:58 +0100

    added loadfile function with error detection

--- Data.cs
+++ Data.cs
@@ -1,8 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 public class Data 
 {
+    public List<string> endListGood;
+    public List<string> endListBad;
+
+    public void LoadFile()
+    {
+        try
+        {
+            string[] lines1 = System.IO.File.ReadAllLines(@"Assets\Text\endtext1.txt");
+            endListBad = new List<string>(lines1);
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(String.Format("Error: The file was not found: '{0}'", e));
+        }
+        catch (IOException e)
+        {
+            Debug.LogError(String.Format("Error: The file could not be opened: '{0}'", e));
+        }
+
+        try
+        {
+            string[] lines2 = System.IO.File.ReadAllLines(@"Assets\Text\endtext2.txt");
+            endListGood = new List<string>(lines2);
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError(String.Format("Error: The file was not found: '{0}'", e));
+        }
+        catch (IOException e)
+        {
+            Debug.LogError(String.Format("Error: The file could not be opened: '{0}'", e));
+        }
+    }
+
     private int mWaterSustainability;
     public int WaterSustainability
     {
