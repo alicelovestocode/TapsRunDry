9efe15065bcddbe63c846c058aaecf31218b2d7a
Alice <aliceevelynashby@gmail.com>
2020-06-05 00:47:04 +0100

    add pitch and volume variation, added stop soundclip function

---
+++ AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour 
+{
+    public AudioMixerGroup audioMixer;
+    public Sound[] sounds;
+    
+	void Awake() 
+    {
+		
+        foreach (Sound s in sounds) 
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+
+            s.source.outputAudioMixerGroup = audioMixer;
+        }
+	}
+
+    public void Play(string name) 
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) 
+        {
+            Debug.LogWarning(String.Format("Error: The sound was not found: '{0}'", name));
+            return;
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+}
